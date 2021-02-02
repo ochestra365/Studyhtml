b1340aeb7771cef721a68baa226b716e5d2d122d
ochestra365 <ochestra365@naver.com>
2021-02-02 15:07:54 +0900

    Masonry_adjusted_added

---
+++ vscode_Practice/Bit.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Gerneric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+       static void Main(string[] args)
+       {
+         int a=1;
+         int b=5;
+         b <<=1;
+       }
+   }
+}
--- vscode_Practice/if.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.collections.Gernerics;
-using System.Linq;
-using System.Text;
-using system.Threading.Tasks;
-
-namespace DHAS
-
-class program
